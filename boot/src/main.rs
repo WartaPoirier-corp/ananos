967fe3843f55fe6986a2b6c635bc4e1e664244bf
Ana Gelez <ana@gelez.xyz>
2021-05-30 21:46:26 +0200

    Better build system

---
+++ boot/src/main.rs
@@ -0,0 +1,37 @@
+use bootloader_locator::locate_bootloader; 
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let bootloader_manifest = locate_bootloader("bootloader").unwrap();
+    
+    let kernel_binary = Path::new("target/x86_64-os/debug/os").canonicalize().unwrap();
+    let kernel_manifest = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent().unwrap()
+        .join("Cargo.toml");
+    let target_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent().unwrap()
+        .join("target");
+    let out_dir = kernel_binary.parent().unwrap();
+
+    // create a new build command; use the `CARGO` environment variable to
+    // also support non-standard cargo versions
+    let mut build_cmd = Command::new(env!("CARGO"));
+
+    // pass the arguments
+    build_cmd.arg("builder");
+    build_cmd.arg("--kernel-manifest").arg(&kernel_manifest);
+    build_cmd.arg("--kernel-binary").arg(&kernel_binary);
+    build_cmd.arg("--target-dir").arg(&target_dir);
+    build_cmd.arg("--out-dir").arg(&out_dir);
+
+    // set the working directory
+    let bootloader_dir = bootloader_manifest.parent().unwrap();
+    build_cmd.current_dir(&bootloader_dir);
+
+    // run the command
+    let exit_status = build_cmd.status().unwrap();
+    if !exit_status.success() {
+        panic!("bootloader build failed");
+    }
+}
